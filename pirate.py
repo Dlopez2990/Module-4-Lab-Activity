2b7ecd4116e28f55cbdff3ad98c6ed80010e552d
Dlopez2990 <139589356+Dlopez2990@users.noreply.github.com>
2023-07-28 15:21:29 -0500

    Create pirate.py

---
+++ pirate.py
@@ -0,0 +1,6 @@
+pirate.py
+greeting = input("Hello, possible pirate! What's the password? ")
+if greeting in ["Arrr!"]: # Added closing square bracket and corrected the quotation mark
+ print("Go away, pirate.")
+else: # Added the missing condition for the 'elif' statement
+ print("Greetings, hater of pirates!")
